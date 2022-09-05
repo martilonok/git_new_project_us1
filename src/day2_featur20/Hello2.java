d7e9b869e4662dcc6abac9f920c6ac7f9d003fff
Aleksandr <martilonok@gmail.com>
2022-09-05 14:27:44 -0500

    Hello 2 on feature 20 branch text is final

--- src/day2_featur20/Hello2.java
+++ src/day2_featur20/Hello2.java
@@ -4,4 +4,6 @@ public class Hello2 {
 
     //created feature 20 branch
     //made a first change on Hello2 file
+    //made another change oin the file
+    //made one more change on the Hello2 that is located on feature 20 branch
 }
